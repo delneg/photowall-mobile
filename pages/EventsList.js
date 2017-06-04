a1ac754f174310efb2402236e3bb8bd490f83587
Denis Bobrov <delneg@Yandex.ru>
2017-06-04 05:16:31 +0300

    Added react native spinner overlay, fixed events loading

--- pages/EventsList.js
+++ pages/EventsList.js
@@ -14,7 +14,7 @@ import {
   AsyncStorage
 } from 'react-native';
 
-
+import Spinner from 'react-native-loading-spinner-overlay';
 // import Button from '../components/Button';
 import Label from '../components/Label';
 import appData from '../app.json'
@@ -32,8 +32,8 @@ export default class HomeScreen extends React.Component {
     };
   }
 
-  componentDidMount() {
-    this._loadInitialState().done();
+
+  _loadInitialData = async () => {
     fetch(`${appData.serverHost}/api/events/`, {
       method: 'GET',
       headers: {
@@ -42,29 +42,20 @@ export default class HomeScreen extends React.Component {
       },
     }).then((response) => response.json())
       .then((responseJson) => {
-        console.log('Success!');
-        this.state.events = responseJson.results;
-        this.state.loaded = true;
+        console.log('Received initial data');
+        this.setState({events: responseJson.results});
+        this.setState({loaded: true});
       })
       .catch((error) => {
         console.log("Error!");
         console.error(error);
       });
-  }
-
-  _loadInitialState = async () => {
-    try {
-      let value = await AsyncStorage.getItem(appData.storageKey);
-      if (value !== null) {
-        this.setState({logged_in: true, token: value});
-        console.log(`Recovered token ${value}`)
-      } else {
-        console.log("Couldn't recover the token");
-      }
-    } catch (error) {
-      console.log('AsyncStorage error: ' + error.message);
-    }
   };
+
+  componentDidMount() {
+    this._loadInitialState().done();
+    this._loadInitialData().done();
+  }
 
 
   static navigationOptions = {
@@ -72,10 +63,14 @@ export default class HomeScreen extends React.Component {
     title: 'Список мероприятий',
   };
 
-
   render() {
     const {navigate} = this.props.navigation;
     const styles = {
+      centering: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+      },
       backgroundImage: {
         flex: 1,
         width: null,
@@ -94,35 +89,21 @@ export default class HomeScreen extends React.Component {
       },
     };
     const stylesheet = StyleSheet.create(styles);
-    let moscow_items = [
-      {image: 'http://placekitten.com/g/500/200', text: 'Мероприятие 1', 'id': 356},
-      {image: 'http://placekitten.com/g/400/200', text: 'Мероприятие 2', 'id': 123},
-      {image: 'http://placekitten.com/g/600/200', text: 'Мероприятие 3', 'id': 32},
-    ];
-    let piter_items = [
-      {image: 'http://placekitten.com/g/700/200', text: 'Мероприятие 8797', 'id': 44},
-      {image: 'http://placekitten.com/g/300/200', text: 'Мероприятие 5', 'id': 56},
-      {image: 'http://placekitten.com/g/800/200', text: 'Мероприятие 6', 'id': 66},
-    ];
-    let lists_obj = [
-      {'items': moscow_items, 'name': 'Москва'},
-      {'items': piter_items, 'name': 'Питер'}
-    ];
     return (
       <Container>
         <Content>
+          <Spinner visible={!this.state.loaded} textContent={"Загрузка..."} textStyle={{color: '#FFF'}}/>
           {this.state.loaded &&
-
-                <List dataArray={this.state.events} renderRow={(data) =>
-                  <ListItem style={styles.listItem}>
-                    <TouchableHighlight style={stylesheet.backgroundImage}
-                                        onPress={() => navigate('EventDetail', {'event': data})}>
-                      <Image source={{uri: data.image_set[0].image}}>
-                        <Text style={styles.hoverText}>{data.name}</Text>
-                      </Image>
-                    </TouchableHighlight>
-                  </ListItem>
-                }/>
+            <List dataArray={this.state.events} renderRow={(data) =>
+              <ListItem style={styles.listItem}>
+                <TouchableHighlight style={stylesheet.backgroundImage}
+                                    onPress={() => navigate('EventDetail', {'event': data})}>
+                  <Image source={{uri: data.image_set[0].image}}>
+                    <Text style={styles.hoverText}>{data.name}</Text>
+                  </Image>
+                </TouchableHighlight>
+              </ListItem>
+            }/>
           }
 
           {!this.state.logged_in &&
